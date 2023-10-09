c43ac4bc8f66b00e2f3838c2b91d0ff48fb3a881
raniaezzo <ezzo.rania@gmail.com>
2023-10-09 19:20:14 +0400

    changing to non-debug mode

--- expLauncher.m
+++ expLauncher.m
@@ -12,8 +12,8 @@ KbName('UnifyKeyNames')
 
 % Initialization
 warning('off');        % do not print warnings
-const.DEBUG = 1;       % skips subject details / data saving
-const.miniWindow = 1;  % for debugging purposes only
+const.DEBUG = 0;       % skips subject details / data saving
+const.miniWindow = 0;  % for debugging purposes only
 const.makemovie =0;   % capture movie of trial (slows down performance)
 
 % Verify that path is correct
